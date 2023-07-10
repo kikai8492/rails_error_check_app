abf8b36c76782f3b1574021d3698b62ac1bdaa2a
shuto kikai <yysskikai04@icloud.com>
2023-07-10 13:45:23 +0900

    secondcommit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,10 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
+    # byebug
+    binding.pry
+    title1 = @blogs[0].title
+    content1 = @blogs[0].content
   end
 
   # GET /blogs/1 or /blogs/1.json
