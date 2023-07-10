aa57b2ba0d1d56db52e36811960f18fd201bbf6d
shuto kikai <yysskikai04@icloud.com>
2023-07-10 17:15:05 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
